88a72d4fb7ed31f39c855e517efd41cafd53117e
SafteyDav3 <talktomedw@gmail.com>
2022-04-03 21:42:11 -0500

    JS fail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 // ←←← Save text field to local storage →→→
+var userData = [{
+    "t-9": "",
+    "t-10": "",
+    "t-11": "",
+    "t-12": "",
+    "t-13": "",
+    "t-14": "",
+    "t-15": "",
+    "t-16": "",
+    "t-17": "",
+}]
+
+console.log(userData)
+// $("#save-9").on('click', function())
+
 // ←←← Display current day at the top of the time-blocks →→→
-
 var nowDate = moment().format("MMMM Do, YYYY");
-
-console.log(nowDate);
-
-
-
-
 $("#currentDay").text(nowDate)
+
+
